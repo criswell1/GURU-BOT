06de96c801960b8fc1f4eb5acf27924731ff48d0
criswell1 <161848115+criswell1@users.noreply.github.com>
2024-03-06 20:28:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2347035869922', 'UNKNOWN', true],
+  ['2347035869922', 'UNKNOWN', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
-
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2347035869922','2347035869922'] 
+global.prems = ['2347035869922', '2347035869922', '2347035869922']
+global.allowed = ['2347035869922', '2347035869922', '2347035869922']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,7 +49,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'UNKNOWN'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
